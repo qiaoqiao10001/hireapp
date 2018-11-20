1965c5b40e70737efe3e682a7bc3e6f2ced6d44d
qiaoqiao10001 <qiaoqiao10001@gmail.com>
2018-11-20 17:53:02 +0800

    init config

--- src/index.js
+++ src/index.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'antd-mobile/dist/antd-mobile.css';
+import {HashRouter,Switch,Route} from 'react-router-dom'
+
+
+import Login from './containers/login'
+import Main from './containers/main'
+import Register from './containers/register'
 
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render((
+    <HashRouter>
+        <Switch>
+            <Route path='/login' component={Login}/>
+            <Route path='/main' component={Main}/>
+            <Route path='/register' component={Register}/>
+        </Switch>
+    </HashRouter>
+
+), document.getElementById('root'));
 
 
